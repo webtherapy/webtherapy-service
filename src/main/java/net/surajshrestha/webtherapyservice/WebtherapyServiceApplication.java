222d97639bb4bd75cd9d47bdd99ed247ba282542
Suraj Shrestha <syuraj@gmail.com>
2021-01-03 20:57:07 -0500

    initial commit

---
+++ src/main/java/net/surajshrestha/webtherapyservice/WebtherapyServiceApplication.java
@@ -0,0 +1,13 @@
+package net.surajshrestha.webtherapyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtherapyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtherapyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/surajshrestha/webtherapyservice/WebtherapyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.surajshrestha.webtherapyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtherapyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
